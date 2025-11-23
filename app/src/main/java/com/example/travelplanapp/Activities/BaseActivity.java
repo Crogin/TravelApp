169447c481cc48339edcc7d0c7f6b5621d55cb55
严浩俊 <3254059519@qq.com>
2025-11-23 23:01:24 +0800

    分支测试？

--- app/src/main/java/com/example/travelplanapp/Activities/BaseActivity.java
+++ app/src/main/java/com/example/travelplanapp/Activities/BaseActivity.java
@@ -3,17 +3,26 @@ package com.example.travelplanapp.Activities;
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.travelplanapp.R;
+
 public class BaseActivity extends AppCompatActivity {
-    //TODO: 基类，但是没开工，后续完善
 
     @SuppressLint("ObsoleteSdkInt")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        //创建时，打印当前Activity名称
+        Log.d("BaseActivity", "onCreate: " + getClass().getSimpleName());
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁时，打印当前Activity名称
+        Log.d("BaseActivity", "onDestroy: " + getClass().getSimpleName());
+    }
 }
